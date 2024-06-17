3a2f9b0d0a23a91b81a7ef3623a62360bdcfd21c
manylovv <yellowmanylov@gmail.com>
2024-06-17 23:35:26 +0500

    tasks exercise

--- test.ts
+++ test.ts
@@ -1,6 +1,39 @@
-const sum = (numbers: number[]) => {
+const todos = [
+  {
+    title: 'Wash dishes',
+    done: false,
+  },
+  {
+    title: 'Read a book',
+    done: false,
+  },
+  {
+    title: 'Go to the walk',
+    done: true,
+  },
+];
+
+const addTask = (newTask: { title: string; done: boolean }) => {
   // write your solution here
 };
 
-const testNumbers = [2, 342, 4102, 4953, 320];
-sum(testNumbers); // should print 9719
+const removeTask = (taskTitle: string) => {
+  // write your solution here
+};
+
+const completeTask = (taskTitle: string) => {
+  // write your solution here
+};
+
+const listAllTasks = () => {
+  // write your solution here
+  // list tasks one by one
+  // if the task is done print
+  //   "✅": "Task title"
+  // if not print
+  //   "-": "Task title"
+};
+
+const listOnlyDoneTasks = () => {
+  // list one by one, should list only if task is done
+};
