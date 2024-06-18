f19611f021afa5c2cbcb76fa8ec7fc37832ec2a9
manylovv <yellowmanylov@gmail.com>
2024-06-18 22:42:56 +0500

    dev script added

--- homework.js
+++ homework.js
@@ -1,14 +1,14 @@
 const tempsByYears = await Bun.file('./1924-2024.json').json();
 
-let year = 1924;
-
-for (let i = 0; i < tempsByYears.length; i++) {
-  console.log(year, tempsByYears[i]);
-
-  year++;
-}
-
 const getMonthsTempsByYear = (year) => {
+  let year = 1924;
+
+  for (let i = 0; i < tempsByYears.length; i++) {
+    console.log(year, tempsByYears[i]);
+
+    year++;
+  }
+
   // return [ 7, 33, 10, 20, -13, 21, -17, 0, -8, 4, 33, -8 ]
 };
 
