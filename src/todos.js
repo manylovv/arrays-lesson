88e7c245164d4f3e6c4b71eb20349fbac816ae79
manylovv <yellowmanylov@gmail.com>
2024-06-18 23:09:33 +0500

    update todos homework

--- src/todos.js
+++ src/todos.js
@@ -1,14 +1,35 @@
-const todos = [
+/*
+how to create new object in js:
+ 
+const newTask = {
+  title: 'some title',
+  done: false
+}
+
+how to change a value in object:
+
+newTask.title = "updated title"
+
+newTask after update will look like that:
+
+{
+  title: 'updated title',
+  done: false
+}
+
+*/
+
+const tasks = [
   {
-    title: "Wash dishes",
+    title: 'Wash dishe',
     done: false,
   },
   {
-    title: "Read a book",
+    title: 'Read a book',
     done: false,
   },
   {
-    title: "Go to the walk",
+    title: 'Go to the walk',
     done: true,
   },
 ];
@@ -26,12 +47,12 @@ const completeTask = (taskTitle) => {
 };
 
 const listAllTasks = () => {
-  for (let i = 0; i < todos.length; i++) {
-    const task = todos[i];
+  for (let i = 0; i < tasks.length; i++) {
+    const task = tasks[i];
     if (task.done) {
-      console.log("✅", task.title);
+      console.log('✅', task.title);
     } else {
-      console.log("-", task.title);
+      console.log('-', task.title);
     }
   }
   // write your solution here
@@ -44,17 +65,82 @@ const listAllTasks = () => {
 
 const listOnlyDoneTasks = () => {
   // list one by one, should list only if task is done
+  // "✅": "Task title"
+};
+
+const editTaskTitle = (taskTitle, updatedTaskTitle) => {
+  // write your solution here
 };
 
 // --------- example:
 
 listAllTasks();
 
-addTask("Play with my dog");
-removeTask("Read a book");
+addTask('Play with my dog');
 
-// listAllTasks();
+/* todos after adding task: 
+[
+  {
+    title: 'Wash dishes',
+    done: false,
+  },
+  {
+    title: 'Read a book',
+    done: false,
+  },
+  {
+    title: 'Go to the walk',
+    done: true,
+  },
+  {
+    title: 'Play with my dog',
+    done: false
+  }
+];
+*/
 
-completeTask("Play with my dog");
+removeTask('Read a book');
+
+/* todos after adding task: 
+[
+  {
+    title: 'Wash dishes',
+    done: false,
+  },
+  {
+    title: 'Go to the walk',
+    done: true,
+  },
+  {
+    title: 'Play with my dog',
+    done: false
+  }
+];
+*/
+
+completeTask('Play with my dog');
+
+/* todos after adding task: 
+[
+  {
+    title: 'Wash dishes',
+    done: false,
+  },
+  {
+    title: 'Go to the walk',
+    done: true,
+  },
+  {
+    title: 'Play with my dog',
+    done: true
+  }
+];
+*/
 
 listOnlyDoneTasks();
+
+// should print to the console
+// ✅: Go to the walk
+// ✅: Play with my dog
+
+editTaskTitle('Wash dishe', 'Wash dishes');
